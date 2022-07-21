e6cd1cc284f977135de3ec47c8e891bdc6c4ad0d
ericmc12 <Eric.al.mc12@gmail.com>
2022-07-21 16:39:49 -0400

    Update app_a.py

--- app_a.py
+++ app_a.py
@@ -34,8 +34,8 @@ st.set_page_config(
      initial_sidebar_state="expanded",
      menu_items={
          'Get Help': 'https://colab.research.google.com/drive/1ZfCMJ55adGJqu2tbIxfFIUHsl0rEdaWn?usp=sharing',
-         'Report a bug': "https://github.com/IBronko/fruit-image-classifier/issues",
-         'About': "# This is a personal project."
+         'Report a bug': "https://github.com/ericmc12/streamlit_tester/issues",
+         'About': "# This is a capstone project."
      }
  )
 
